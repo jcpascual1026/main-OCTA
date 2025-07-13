8bd8bc612ebe79344a434a4712b214020cb90496
jcpascual1026 <130044225+jcpascual1026@users.noreply.github.com>
2025-07-13 11:27:50 +0800

    innitial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,202 @@
+<script>
+
+</script>
+
+
+<div class=" relative z-0 bg-white mb-[10vh]">
+
+  <div class="relative z-10 flex flex-col lg:flex-row items-center justify-center text-center lg:text-left px-6 py-12 lg:p-[10vh_20vh] gap-10">
+    <img src="/images/lines.png" alt="Decorative lines" class="absolute inset-0 z-0 mb-10 h-[140vh] sm:h-[150vh] md:h-[180vh] lg:h-[250vh] object-none object-[100%_-200%] sm:object-[10%_90%] md:object-[10%_85%] lg:object-[5%_-10%]"/>
+    <div class="flex-1">
+      <h1 class="text-[2.8rem] sm:text-[2rem] lg:text-[2.9rem] font-bold leading-tight w-full lg:w-4/5">
+        <span class="text-[#D9392B]">Organization</span> for<br class="lg:hidden" />
+        <span class="text-[#1f1f1f]">Computing and Technology</span><br class="lg:hidden" />
+        <span class="text-[#1f1f1f] lg:hidden">Advancement</span>
+        <span class="hidden lg:inline">Advancement</span>
+      </h1>
+      <p class="mt-4 text-[#444] text-sm lg:text-base font-medium lg:font-bold">
+        Unite. Innovate. Lead the Future of Technology.
+      </p>
+      <button class="mt-6 lg:mt-[5vh] px-10 lg:px-14 py-3 rounded-2xl text-white bg-[#D9392B] font-semibold text-base shadow-md active:translate-y-1 cursor-pointer">
+        Sign in
+      </button>
+    </div>
+    
+    <div class="relative flex-1 flex justify-center items-center mt-10 lg:mt-0">
+      <img src="/images/bg.png" alt="bg" class="w-full max-w-[80vh] relative z-[1] hidden lg:block" />
+      <img src="/images/ccs.png" alt="ccs" class="absolute z-[2] hidden lg:block w-[45vh] top-[5%] left-[38%]" />
+      <img src="/images/octa.png" alt="octa" class="absolute z-[3] hidden lg:block w-[45vh] top-[35%] right-[40%]" />
+    </div>
+  </div>
+</div>
+
+
+<!-- Ok n to -->
+<div class="w-11/12 max-w-7xl mx-auto my-50 text-center px-4">
+  <h1 class="sm:text-4xl font-bold text-[1.5rem]">
+    <span class="text-[#D9392B] lg:text-md">What to expect</span> when you join OCTA
+  </h1>
+  
+  <h4 class="text-[#F06336] w-full sm:w-4/5 lg:w-2/3 mx-auto mt-4 mb-20 font-bold text-[0.9rem] sm:text-lg">
+    At OCTA, you're joining a dynamic community driven by technological advancement and creative innovation!
+  </h4>
+
+  <img src="/images/line2.png" alt="Decorative background" class="w-[70%] mx-auto my-10 object-contain hidden lg:block z-0"/>
+
+  <div class="flex flex-col lg:flex-row gap-10 items-center justify-center lg:mt-[-30vh]">
+    <div class="flex-1 text-center">
+      <img src="/images/collaborative.png" alt="collab" class="w-24 sm:w-[20vh] mx-auto mb-4" />
+      <h3 class="font-bold text-lg">Collaborative Community</h3>
+      <p class="w-4/5 mx-auto text-sm sm:text-base">
+        Work with fellow students and tech enthusiasts in a creative and supportive environment.
+      </p>
+    </div>
+
+    <div class="flex-1 text-center">
+      <img src="/images/showcaseyourwork.png" alt="show" class="w-24 sm:w-[20vh] mx-auto mb-4" />
+      <h3 class="font-bold text-lg">Showcase Your Work</h3>
+      <p class="w-4/5 mx-auto text-sm sm:text-base">
+        Build your portfolio and get your designs featured on OCTA’s platforms.
+      </p>
+    </div>
+
+    <div class="flex-1 text-center">
+      <img src="/images/handsonproject.png" alt="handson" class="w-24 sm:w-[20vh] mx-auto mb-4" />
+      <h3 class="font-bold text-lg">Hands-On Projects</h3>
+      <p class="w-4/5 mx-auto text-sm sm:text-base">
+        Gain real-world experience by designing for OCTA initiatives and platforms.
+      </p>
+    </div>
+  </div>
+</div>
+
+
+<div class="w-full bg-[#343235] py-16 px-4">
+  <div class="bg-[url('/images/border.png')] bg-no-repeat bg-contain bg-center w-[95%] mx-auto py-12">
+
+    <div class="relative max-w-5xl mx-auto bg-white rounded-[1rem_10rem_1rem_10rem] 
+                px-4 py-10 lg:px-16 flex flex-col lg:flex-row items-center gap-10 
+                shadow-xl overflow-hidden">
+
+      <div class="relative z-10 sm:flex-row items-center justify-center gap-4 sm:mr-[2%]">
+        <img src="/images/ALGO.png" alt="ALGO" class="w-20 sm:w-35" />
+        <img src="/images/BSIT.png" alt="BSIT" class="w-20 -mt-4 ml-15 sm:w-35 sm:-mt-4 sm:ml-15" />
+      </div>
+
+      <div class="text-left relative z-10 max-w-xl">
+        <h2 class="text-2xl sm:text-3xl lg:text-4xl font-bold mb-4">
+          <span class="text-[#D9392B]">Explore</span> Our <br />
+          <span class="text-[#1e1e1e]">Sub-Organizations</span>
+        </h2>
+        <p class="text-[#444] mb-6 text-sm sm:text-base">
+          Whether you're passionate about building tech solutions or mastering algorithms,
+          there's a place for you at OCTA. Join BSIT or ALGO today and take your skills to the next level!
+        </p>
+        <button class="px-6 py-3 rounded-xl text-white bg-[#D9392B] font-semibold text-base shadow-md active:translate-y-1 cursor-pointer">
+          Learn more <i class="fa-solid fa-arrow-up-right-from-square ml-2"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <div class="my-20 text-center">
+    <h2 class="text-white text-3xl sm:text-4xl lg:text-5xl font-bold mb-8">
+      CCS FEST <span class="text-[#D9392B]">2024</span>
+    </h2>
+    <img src="/images/fest.png" alt="CCS Fest 2024" class="w-4/5 sm:w-3/5 lg:w-1/2 mx-auto" />
+  </div>
+</div>
+
+
+
+<div class="news-galery w-11/12 mx-auto pt-20">
+  <div class="text-center mb-20">
+    <h1 class="text-[3.2rem] font-bold">OCTA Newsletter</h1>
+    <p class="text-[#D9392B] font-medium">Latest initiative, student projects, or upcoming events.</p>
+  </div>
+  <div class="flex flex-col lg:flex-row gap-8">
+    <div class="flex flex-col gap-8 lg:pr-8 lg:border-r-4 border-black">
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news4.png" alt="news4" class="w-full object-cover" />
+        <h6 class="bg-[#36B2F0] text-white rounded-md px-2 w-fit text-center my-2">Event</h6>
+        <h1>CCS FEST 2024</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news1.png" alt="news1" class="w-full object-cover" />
+        <h6 class="bg-[#DB3A34] text-white rounded-md px-2 w-fit text-center my-2">News</h6>
+        <h1>STUDENT FAIR</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-8">
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news5.png" alt="news5" class="w-full object-cover" />
+        <h6 class="bg-[#36B2F0] text-white rounded-md px-2 w-fit text-center my-2">Event</h6>
+        <h1>COSPLAY FEST 2023</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news3.png" alt="news3" class="w-full object-cover" />
+        <h6 class="bg-[#DB3A34] text-white rounded-md px-2 w-fit text-center my-2">News</h6>
+        <h1>ECHO Recruit</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-8 lg:pl-8 lg:border-l-4 border-black">
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news6.png" alt="news6" class="w-full object-cover" />
+        <h6 class="bg-[#F79733] text-white rounded-md px-2 w-fit text-center my-2">Project</h6>
+        <h1>Web Training</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+      <div class="pb-8 border-b-4 border-black">
+        <img src="/images/news2.png" alt="news2" class="w-full object-cover" />
+        <h6 class="bg-[#36B2F0] text-white rounded-md px-2 w-fit text-center my-2">Event</h6>
+        <h1>CCS FEST 2024</h1>
+        <p class="w-full lg:w-[70%]">Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="orangebg bg-[#F06336] w-11/12 mx-auto my-[10vh] rounded-md">
+  <div class="flex flex-col lg:flex-row p-6 gap-6">
+    <img src="/images/fp-example.png" alt="example" class="max-w-[80vh] w-full mx-auto" />
+    <div class="text-white px-4 py-6 text-left">
+      <h4 class="font-bold">Your Voice. Your Space.</h4>
+      <h1 class="text-[2.1rem] font-bold mb-4">FREEDOM WALL</h1>
+      <p class="mb-8 w-[60%] text-[1rem]">The OCTA Freedom Wall is a dedicated online space where students of the College of Computer Studies can freely share their thoughts, ideas, and experiences.</p>
+      <button class="px-8 py-3 rounded-lg text-[#D9392B] bg-white font-semibold shadow-md active:translate-y-1 cursor-pointer">
+        Create a message
+      </button>
+    </div>
+  </div>
+</div>
+
+<div class="ready-to-join text-center px-4">
+  <h1 class="text-3xl font-bold">Ready to join <span class="text-[#D9392B]">OCTA</span>?</h1>
+  <h2 class="w-full lg:w-2/5 mx-auto text-[#B6B6B6] my-6 text-[1.4rem]">Join us now and unlock endless opportunities in technology and collaboration!</h2>
+  <button class="px-14 py-3 rounded-lg text-white bg-[#D9392B] font-semibold shadow-md active:translate-y-1 cursor-pointer">
+    Join OCTA
+  </button>
+</div>
+
+<div class="offerupdate hidden md:flex w-11/12 mx-auto my-40 p-6 lg:p-12 border-[5px] border-black rounded-md flex-col lg:flex-row items-center gap-8">
+  <h2 class="w-full lg:w-3/5">OCTA offers exclusive updates on events, projects, and opportunities.</h2>
+  <form class="w-full max-w-lg">
+    <p>Subscribe to our newsletter so you never miss an update.</p>
+    <div class="flex items-center mt-4 border-2 border-black rounded-md p-2">
+      <input type="search" placeholder="Your Email" class="w-full bg-transparent border-none outline-none focus:outline-none focus:ring-0 focus:border-transparent"/>
+      <i class="fa-solid fa-arrow-right text-white bg-black p-2 rounded-md ml-2"></i>
+    </div>
+  </form>
+</div>
+
+<style global>
+  body {
+    overflow-x: hidden;
+  }
+</style>
